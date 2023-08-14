46d2fe20af2142a4d83745c22b2883d6ac4dda25
razvanJS <razvanvasile20091@gmail.com>
2023-08-14 23:11:00 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [comenzi, setComezi] = useState('')
+  const [realocari, setRealocari] = useState('')
+  const [rata, setRata] = useState('')
+  const [realocariPosibile, setRealocariPosibile] = useState('')
+  const [prag, setPrag] = useState('')
+
+
+  useEffect(() => {
+    const rata = +((realocari * 100) / comenzi).toFixed(2)
+    setRata(rata)
+    setPrag(9)
+    if (rata >= prag) {
+      setRealocariPosibile(0)
+    }
+    else {
+      const diferenta = rata - prag;
+
+      setRealocariPosibile(Math.abs(comenzi * diferenta / 100).toFixed(0))
+
+    }
+
+
+  }, [comenzi, realocari])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="App-table">
+        <form className='table-form'>
+          <label>Comenzi: </label>
+          <input key='comenzi' onChange={
+            (e) => {
+              setComezi(+e.target.value)
+            }
+          } type='text' name="comenzi" value={comenzi} required maxLength="5"></input><br></br>
+          <label>Realocari: </label>
+          <input onChange={(e) => {
+            setRealocari(+e.target.value)
+
+
+          }} type='text' name="realocari" value={realocari} required maxLength="5"></input>
+          <br></br>
+          <label>Realocari Disponibile: </label>
+          <input disabled type='text' value={realocariPosibile} name='realocariPosibile'  ></input>
+          <br></br>
+          <label>Rata Alocare: </label>
+          <input type='text' disabled name='rata' value={rata} ></input>
+          <label>% </label>
+          <input id="sub" type='submit'></input>
+
+        </form>
+
+      </div>
     </div>
   );
 }
